fbed81d2bcd871e1ce42bacd1203b866b455f99b
adesoft <adesholly11@gmail.com>
2021-10-13 17:44:56 +0000

    fizing the line

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ function drawLine(x1, y1, x2, y2){
    ctx.beginPath();
    ctx.moveTo(x1, y1);
    ctx.lineTo(x2, y2);
-   ctx.lineWidth = size;
+   ctx.lineWidth = size * 2;
    ctx.strokeStyle = color;
    ctx.stroke();
 }
